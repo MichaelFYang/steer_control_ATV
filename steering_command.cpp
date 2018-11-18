87da999fda93e7343004f82d24b568e06de649d6
MichaelFYang <fanyang2@andrew.cmu.edu>
2018-11-17 23:28:16 -0500

    Circle Trajectory

--- steering_command.cpp
+++ steering_command.cpp
@@ -2,12 +2,22 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 
-const int num_commands = 100;
+const int num_commands = 401;
 
 float* CommandList(){
 	static float angleList[num_commands];
-	for (int i=0; i<num_commands; i++){
-		angleList[i] = -200 + 2*i;
+	int num = (num_commands-1)/4;
+	for (int i=0; i<num; i++){
+		angleList[i] = -2*i;
+	}
+	for (int i=0; i<num; i++){
+		angleList[num+i] = -200 + 2*i;
+	}
+	for (int i=0; i<num; i++){
+		angleList[2*num+i] = 2*i;
+	}
+	for (int i=0; i<=num; i++){
+		angleList[3*num+i] = 200 - 2*i;
 	}
 	return angleList;
 }
@@ -17,10 +27,10 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "steer_command");
 	ros::NodeHandle n;
 	ros::Publisher command_pub = n.advertise<std_msgs::Float32>("ros_talon/steering_angle", 100);
-	ros::Rate loop_rate(1);
+	ros::Rate loop_rate(5);
 	float* angleList = CommandList(); 
 	int count = 0;
-	while (ros::ok() && count < 100)
+	while (ros::ok() && count < num_commands)
 	{
 		std_msgs::Float32 angle_des;
 		float angle = angleList[count];
